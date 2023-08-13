c023f2889bdc30f614e4ae5b7bd587703e7e673b
B.Rasmini devi <121505507+22A91A61E1@users.noreply.github.com>
2023-08-13 08:21:31 +0530

    import java.util.Scanner; public class wow {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         //int a=sc.nextInt();         char c=sc.next().charAt(0);         int b=c;         System.out.println(b);     } }

---
+++ Find_ASCII_Value_of_a_Character.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class wow
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        //int a=sc.nextInt();
+        char c=sc.next().charAt(0);
+        int b=c;
+        System.out.println(b);
+    }
+}
