b34af54ee3acba7b784445106cdaa20afe2834c1
B.Rasmini devi <121505507+22A91A61E1@users.noreply.github.com>
2023-09-06 21:01:30 +0530

    import java.util.*; public class absolute {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int N=sc.nextInt();         int arr[]=new int[N];         for(int i=0;i<N;i++)         {             arr[i]=sc.nextInt();         }         int sum=0;         int diff=0;         for(int i=0;i<N;i++)         {
                if((i%2)==0)
                {
                    sum=sum+arr[i];
                }
                else
                {
                    diff=diff+arr[i];
                }
               
            }
            System.out.println(Math.abs(sum-diff));
        }
    }

---
+++ Absolute_difference_b/w_sum_of_even_and_sum_of_odd_indexed_elements.java
@@ -0,0 +1,30 @@
+import java.util.*;
+public class absolute
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int N=sc.nextInt();
+        int arr[]=new int[N];
+        for(int i=0;i<N;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        int sum=0;
+        int diff=0;
+        for(int i=0;i<N;i++)
+        {
+            
+            if((i%2)==0)
+            {
+                sum=sum+arr[i];
+            }
+            else
+            {
+                diff=diff+arr[i];
+            }
+           
+        }
+        System.out.println(Math.abs(sum-diff));
+    }
+}
