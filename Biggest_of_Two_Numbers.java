6612d794df6d54f334d4086b34b9f2e4a4e2ab5a
B.Rasmini devi <121505507+22A91A61E1@users.noreply.github.com>
2023-10-06 18:44:24 +0530

    import java.util.*; public class bigg {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int T=sc.nextInt();         int X=sc.nextInt();         if(T>X)         {             System.out.print(T);         }         else         {             System.out.print(X);         }     } }

---
+++ Biggest_of_Two_Numbers.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class bigg
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int T=sc.nextInt();
+        int X=sc.nextInt();
+        if(T>X)
+        {
+            System.out.print(T);
+        }
+        else
+        {
+            System.out.print(X);
+        }
+    }
+}
